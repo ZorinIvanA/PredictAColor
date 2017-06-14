121ecb02d9fb26c875d93979962a35db38eb4345
Зорин Иван Андреевич <zorin.ivan@outlook.com>
2017-06-14 23:59:37 +0300

    #8 созданы базовый класс для ViewModel и базовый класс для команд

---
+++ PredictAColor/PredictAColor/MVVM/CommandBase.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace PredictAColor.MVVM
+{
+    /// <summary>
+    /// Базовый класс для команд
+    /// </summary>
+    public abstract class CommandBase : ICommand
+    {
+        /// <summary>
+        /// Событие, которое происходит, когда сработал метод CanExecute
+        /// </summary>
+        public event EventHandler CanExecuteChanged;
+
+        /// <summary>
+        /// Действие, которое выполняет команда
+        /// </summary>
+        protected Action<Object> _actionToExecute;
+
+        /// <summary>
+        /// Конструктор, в который передаётся действие
+        /// </summary>
+        /// <param name="actionToExecute">Действие, которое должна выполнять команда</param>
+        public CommandBase(Action<Object> actionToExecute)
+        {
+            _actionToExecute = actionToExecute;
+        }
+
+        private Boolean _canExecute;
+        /// <summary>
+        /// Метод, который определяет возможность выполнения команды
+        /// </summary>
+        /// <param name="parameter">Некий параметр</param>
+        /// <returns>Может ли быть выполнена команда</returns>
+        public virtual bool CanExecute(Object parameter)
+        {
+            if (_canExecute != (_actionToExecute != null))
+            {
+                var cec = CanExecuteChanged;
+                if (cec != null)
+                    cec(this, EventArgs.Empty);
+
+                _canExecute = _actionToExecute != null;
+            }
+            return _actionToExecute != null;
+        }
+
+        /// <summary>
+        /// Непосредственно выполнение команды
+        /// </summary>
+        /// <param name="parameter"></param>
+        public void Execute(object parameter)
+        {
+            _actionToExecute(parameter);
+        }
+    }
+}
---
+++ PredictAColor/PredictAColor/MVVM/ViewModelBase.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PredictAColor.MVVM
+{
+    /// <summary>
+    /// Базовый класс для всех моделей представления
+    /// </summary>
+    public abstract class ViewModelBase : INotifyPropertyChanged
+    {
+        /// <summary>
+        /// Событие, которое сообщает об изменении свойства
+        /// </summary>
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        /// <summary>
+        /// Вызывает PropertyChanged для одного свойства
+        /// </summary>
+        /// <param name="propertyName">Свойство, об изменениях в котором надо сообщить</param>
+        protected void RaisePropertyChanged(String propertyName)
+        {
+            var pc = PropertyChanged;
+            if (pc != null)
+                pc(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        /// <summary>
+        /// Вызывает PropertyChanged для всех свойств
+        /// </summary>
+        protected void RaiseAllPropertiesChanged()
+        {
+            foreach (var property in this.GetType().GetProperties())
+            {
+                RaisePropertyChanged(property.Name);
+            }
+        }
+
+        public ViewModelBase()
+        {
+
+        }
+
+        public ViewModelBase(params object[] args)
+        {
+
+        }
+    }
+}
